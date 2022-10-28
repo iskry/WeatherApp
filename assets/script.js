e979d86ac9b76dec4f3c4470175b6baf1d18e33f
iskry <refocuslucifer@protonmail.com>
2022-10-27 22:27:15 -0500

    converted dynamic elements into javascript

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var api_key = "7ee0556fd27777e1ec83e88a645a43fd"
 var increm = (localStorage.getItem('index')) ? localStorage.getItem('index'):0
 var searchHistory = document.getElementById("searchHistory")
+var historyContainer = document.getElementById("histContainer")
+var weatherCardsContainer = document.getElementById("weatherCards")
 
 historyHandler()
 
@@ -9,7 +11,8 @@ document.getElementById("searchBtn").addEventListener("click", function(event) {
     weatherData()
 })
 
-function weatherData() {
+function weatherData(history) {
+    console.log(history)
     var cityName = document.getElementById("searchInput").value
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${api_key}`)
     .then(function (response) {
@@ -27,6 +30,25 @@ function weatherData() {
             localStorage.setItem(increm, data["city"].name)
             console.log(data)
             historyHandler()
+            for (i = 0; i <= 6; i++) {
+
+                var weatherCardDiv = document.createElement("div")
+                weatherCardsContainer.appendChild(weatherCardDiv)
+                weatherCardDiv.classList.add("card")
+                weatherCardDiv.classList.add("m-3")
+
+                var weatherCardBodyDiv = document.createElement("div")
+                weatherCardDiv.appendChild(weatherCardBodyDiv)
+                weatherCardBodyDiv.classList.add("card-body")
+                var weatherData = data.list[i]
+                weatherCardBodyDiv.append(weatherData.dt_txt)
+                weatherCardBodyDiv.classList.add("bg-light")
+                weatherCardBodyDiv.style.minWidth = "200px"
+                weatherCardBodyDiv.style.minHeight = "100px"
+
+                
+            }   
+            
         })
     }
     )
@@ -34,25 +56,28 @@ function weatherData() {
 
 
 function historyHandler() {if (localStorage.length > 0) {
+    historyContainer.innerText = ""
     var lineIndex = 5
     var startHis = localStorage.index 
     var endHis = localStorage.index - 5
+
     for (startHis + 1; startHis > endHis; startHis--) {
-    lineIndex--
-    var lineItem = document.getElementById("history" + lineIndex)
-    var searchHistoryItem = localStorage[localStorage.getItem("index") - lineIndex]
-    if (searchHistoryItem == undefined) {
-        lineItem.append("")
-    } else {
+        lineIndex--
+        var searchHistoryItem = localStorage[localStorage.getItem("index") - lineIndex]
+        var lineItem = document.createElement("button")
+        historyContainer.appendChild(lineItem)
         document.getElementById("prevSearchesHeader").classList.remove("invisible")
-        lineItem.textContent = ""
-        lineItem.classList.add("p-3")
-        lineItem.classList.add("m-2")
-        lineItem.classList.add("w-100")
         lineItem.classList.add("bg-dark")
         lineItem.classList.add("text-white")
-        lineItem.append(searchHistoryItem)
+        lineItem.classList.add("btn")
+        lineItem.classList.add("m-1")
+        lineItem.classList.add("p-2")
+        lineItem.innerText = searchHistoryItem
     }
-    }}
+}
 }
 
+
+function populateWeatherData() {
+
+}
