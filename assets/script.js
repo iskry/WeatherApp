adc004ba1b8e6c998c4b24f58bd8e98e278d0c92
iskry <refocuslucifer@protonmail.com>
2022-10-28 17:55:53 -0500

    fixed history

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,17 @@ var searchHistory = document.getElementById("searchHistory")
 var historyContainer = document.getElementById("histContainer")
 var weatherCardsContainer = document.getElementById("weatherCards")
 
-
-// to do: fix history links
 // use moment js to convert dates and then filter to 5 days
 // use blob to pull icon? 
 
-historyHandler()
+
 
 document.getElementById("searchBtn").addEventListener("click", function(event) {
     event.preventDefault()
     weatherData()
 })
 
-function weatherData(history) {
-    console.log(history)
+function weatherData() {
     var cityName = document.getElementById("searchInput").value
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${api_key}`)
     .then(function (response) {
@@ -26,17 +23,39 @@ function weatherData(history) {
         if (data[0] == undefined) {
             alert("Please input a city")
         } else
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&units=imperial&appid=${api_key}`)
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&exclude=minutely,hourly,alerts&appid=${api_key}`)
         .then(function (response) {
             return response.json()
         }).then(function (data) {
             increm++
             localStorage.setItem("index", increm)
             localStorage.setItem(increm, data["city"].name)
-            console.log(data)
-            historyHandler()
-            for (i = 0; i <= 6; i++) {
+            var recentInput = document.createElement("button")
 
+            document.getElementById("sessionSearchesHeader").appendChild(recentInput)
+            document.getElementById("sessionSearchesHeader").classList.remove("invisible")
+            recentInput.classList.add("bg-dark")
+            recentInput.classList.add("text-white")
+            recentInput.classList.add("btn")
+            recentInput.classList.add("m-1")
+            recentInput.classList.add("p-2")
+            recentInput.setAttribute('id', "history" + i)
+            recentInput.innerText = data.city.name
+            
+            var HistoryButtons = document.querySelectorAll("button:not(#searchBtn)")
+
+            HistoryButtons.forEach(function(button) {
+                button.addEventListener("click", function() {
+                    document.getElementById("searchInput").value = button.innerText
+                })                
+            })
+
+
+            
+            console.log(data)
+            weatherCardsContainer.innerText = ""
+            for (i = 0; i <= 6; i++) {
+                
                 var weatherCardDiv = document.createElement("div")
                 weatherCardsContainer.appendChild(weatherCardDiv)
                 weatherCardDiv.classList.add("card")
@@ -59,30 +78,38 @@ function weatherData(history) {
     )
 }
 
+for (var i = localStorage.index;  i > 0 ; i--) {
+    if (i === localStorage.index - 5) {
+        break
+    } else
+    {
+    console.log(i)
+    console.log(localStorage[i])
+    var searchHistoryItem = localStorage[i]
 
-function historyHandler() {if (localStorage.length > 0) {
-    historyContainer.innerText = ""
-    var lineIndex = 5
-    var startHis = localStorage.index 
-    var endHis = localStorage.index - 5
+    var lineItem = document.createElement("button")
+    historyContainer.appendChild(lineItem)
+    document.getElementById("prevSearchesHeader").classList.remove("invisible")
+    lineItem.classList.add("bg-dark")
+    lineItem.classList.add("text-white")
+    lineItem.classList.add("btn")
+    lineItem.classList.add("m-1")
+    lineItem.classList.add("p-2")
+    lineItem.setAttribute('id', "history" + i)
+    lineItem.innerText = searchHistoryItem
 
-    for (startHis + 1; startHis > endHis; startHis--) {
-        lineIndex--
-        var searchHistoryItem = localStorage[localStorage.getItem("index") - lineIndex]
-        var lineItem = document.createElement("button")
-        historyContainer.appendChild(lineItem)
-        document.getElementById("prevSearchesHeader").classList.remove("invisible")
-        lineItem.classList.add("bg-dark")
-        lineItem.classList.add("text-white")
-        lineItem.classList.add("btn")
-        lineItem.classList.add("m-1")
-        lineItem.classList.add("p-2")
-        lineItem.innerText = searchHistoryItem
+    var allHistoryButtons = document.querySelectorAll("button:not(#searchBtn)")
+
+    allHistoryButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            document.getElementById("searchInput").value = button.innerText
+        })
+        
     }
-}
+    )
+} 
+
 }
 
 
-function populateWeatherData() {
 
-}
